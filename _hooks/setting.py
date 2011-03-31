791f5be79d9b7b3b8103db0cc9e8595766807691
Waylan Limberg <waylan@gmail.com>
2011-03-31 01:09:03 -0400

    Added syntax highlighting with Highlight.js.

--- _hooks/setting.py
+++ _hooks/setting.py
@@ -13,8 +13,38 @@ Site.CONTEXT.blog = AttrDict(
 Site.IGNORE += ('README', 'growl.py')
 #Site.DEPLOY_DIR = path.abspath('/home/grebmill/webapps/achinghead/')
 
+""" --- Set up Markdown --- """
+
+from markdown.extensions.codehilite import CodeHilite
+
+class JSliter(markdown.treeprocessors.Treeprocessor):
+    """ Prepare code blocks for a JS code highlighter. """
+
+    def run(self, root):
+        """ Find code blocks, id language and set as class. """
+        blocks = root.getiterator('pre')
+        for block in blocks:
+            children = block.getchildren()
+            if len(children) == 1 and children[0].tag == 'code':
+                code = children[0]
+                ch = CodeHilite(code.text)
+                ch._getLang()
+                if ch.lang:
+                    code.set('class', 'language-%s' % ch.lang)
+                    code.text = '%s\n' % ch._escape(ch.src)
+
+
+class JSliterExt(markdown.Extension):
+    """ Add JSliter to Markdown instance. """
+
+    def extendMarkdown(self, md, md_globals):
+        liter = JSliter(md)
+        md.treeprocessors.add('hilite', liter, '_begin')
+
+
+
 Config.transformers['md'] = functools.partial(
                                     markdown.markdown,
-                                    extensions = ['extra'])
+                                    extensions = ['extra', JSliterExt()])
 
 Config.transformers['markdown'] = Config.transformers['md']
